cdc6b34c4358aafda855bc605fc04d8ed0f53ec6
singhnitin88 <nitin.singh@razorpay.com>
2018-01-05 14:48:33 +0530

    -Added deploy scripts

---
+++ lambda-deploy/index.js
@@ -0,0 +1,102 @@
+'use strict';
+const request = require('request');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const zlib = require('zlib');
+
+const path = 'static/';
+const bucketName = "zenandmotorcycles.com";
+
+const mailMessage = `Deploy to ${bucketName}/${path} succeeded!`;
+const mailSubject = 'Deploy succeeded';
+
+const sns = new AWS.SNS();
+const s3 = new AWS.S3({
+  params: {
+    Bucket: bucketName
+  }
+});
+
+const computeContentType = (filename) => {
+  const parts = filename.split('.');
+  switch (filename.split('.')[parts.length-1]) {
+    case 'png':
+      return "image/png";
+    case 'html':
+      return "text/html";
+    case 'js':
+      return "application/javascript";
+    case 'css':
+      return "text/css";
+  }
+};
+
+const putFileToS3 = (fileObject) => new Promise((resolve, reject) => {
+  const gzip = zlib.createGzip();
+
+  console.log(fileObject.name)
+
+  const publicFolderName = "/public/"
+  const start = fileObject.download_url.indexOf(publicFolderName) + publicFolderName.length
+  const filePath =  fileObject.download_url.substring(start);
+
+  request(fileObject.download_url)
+    .pipe(gzip)
+    .pipe(fs.createWriteStream(`/tmp/${fileObject.name}`))
+    .on('finish', () => {
+      s3.upload({
+        Bucket: bucketName,
+        Key: filePath,
+        Body: fs.createReadStream(`/tmp/${fileObject.name}`),
+        ACL: 'public-read',
+        CacheControl: 'max-age=31536000',
+        ContentType: computeContentType(fileObject.name),
+        ContentEncoding: 'gzip',
+      }, (error) => {
+        if (error) return reject();
+        else return resolve();
+      });
+    });
+});
+
+var filesToDownload = []
+var foldersToDownload = []
+const downloadFolderFromGithub = (downloadUrl, callback) => {
+  request({
+    uri: downloadUrl,
+    headers: {
+      'User-Agent': 'AWS Lambda Function' // Without that Github will reject all requests
+    }
+  }, (error, response, body) => {
+    JSON.parse(body).forEach((fileObject) => {
+      if(fileObject.type == 'file'){
+        filesToDownload.push(fileObject)
+      } else if(fileObject.type == 'dir'){
+        foldersToDownload.push(fileObject.url)
+      }
+    });
+
+    if(foldersToDownload.length > 0){
+      const folderUrl = foldersToDownload[0]
+      foldersToDownload.splice(0, 1)
+      downloadFolderFromGithub(folderUrl, callback)
+    } else{
+      console.log("Files to download ========= ")
+      filesToDownload.forEach((fileObject) => {
+        console.log(fileObject.name)
+      })
+      callback()
+    }
+  });
+}
+
+exports.handler = (event, context, callback) => {
+  const downloadsUrl = "https://api.github.com/repos/singhnitin88/zenandmotorcycles/contents/public"
+
+  downloadFolderFromGithub(downloadsUrl, () => {
+    console.log("Downloading from Github and Uploading to S3 ======== ")
+    filesToDownload.forEach((fileObject) => {
+        putFileToS3(fileObject)
+      })
+  })
+};
---
+++ lambda-deploy/readme.txt
@@ -0,0 +1,2 @@
+Most of this is picked up from here:
+https://medium.com/@rafalwilinski/use-aws-lambda-sns-and-node-js-to-automatically-deploy-your-static-site-from-github-to-s3-9e0987a073ec
